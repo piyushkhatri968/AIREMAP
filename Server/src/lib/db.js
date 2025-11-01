29bc84e1d29e8adfbe0783a64b6ec5b328508c47
piyushkhatri968 <piyushkhatri968@gmail.com>
2025-11-02 03:18:58 +0500

    .

--- Server/src/lib/db.js
+++ Server/src/lib/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`Database connected to: ${conn.connection.host}`);
   } catch (error) {
     console.log("Error in connecting to mongodb:", error);
